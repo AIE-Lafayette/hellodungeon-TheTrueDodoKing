db5ebe729452c7e620abe5f3e7f79ea2aa12d7fa
Hunter Rachal <thetruedodoking@gmail.com>
2023-08-23 15:08:09 -0500

    start of player stats and introduction

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,7 +10,28 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //player name
+            string playerTrueName = "Insert Player";
+            string playerName = "Henry";
+            //player stats
+            int maxHealth = 100;
+            bool playerAlive = true;
+            float playerCoins = 10.25f;
+            int playerDmg = 1;
+            float playerMana = 50.00f;
 
+
+            //Introduction
+            Console.WriteLine("Hello there");
+            Console.WriteLine("What is your name again?");
+            //player creates there name
+            playerTrueName = Console.ReadLine();
+            //
+            Console.WriteLine(playerTrueName + " really? Thats what you chose to call your self? Why not " + playerName + "?");
+
+            Console.WriteLine("Would you like to be renamed 'Henry'? y/n");
+            Console.ReadLine();
+            Console.WriteLine("Henry it is! That better isn't it? Welcome to the start of your Journey");
         }
     }
 }
