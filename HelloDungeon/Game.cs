ae53d8729c1c58449c030442e400707ffe9c7c2b
Hunter Rachal <thetruedodoking@gmail.com>
2023-08-25 12:00:07 -0500

    Complete overhall of code new start (Karts story)

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,28 +10,39 @@ namespace HelloDungeon
     {
         public void Run()
         {
-            //player name
-            string playerTrueName = "Insert Player";
-            string playerName = "Henry";
-            //player stats
-            int maxHealth = 100;
-            bool playerAlive = true;
-            float playerCoins = 10.25f;
-            int playerDmg = 1;
-            float playerMana = 50.00f;
-
-
-            //Introduction
-            Console.WriteLine("Hello there");
-            Console.WriteLine("What is your name again?");
-            //player creates there name
-            playerTrueName = Console.ReadLine();
+            //player character choices
             //
-            Console.WriteLine(playerTrueName + " really? Thats what you chose to call your self? Why not " + playerName + "?");
 
-            Console.WriteLine("Would you like to be renamed 'Henry'? y/n");
-            Console.ReadLine();
-            Console.WriteLine("Henry it is! That better isn't it? Welcome to the start of your Journey");
+            //Samson the Tank/Melee (simple high defenese & hp)
+            string playerName = "Samson";
+            float maxHealthSam = 25;
+            float dmgSam = 3;
+
+            //Veronica the Mage/Academic
+
+            float maxHealthVer = 15;
+            float mana = 50;
+            float dmgVer = 5;
+
+            //Melina the Archer/Hunter (ammo and crit chance)
+
+            float maxHealthMel = 20;
+            float dmgMel = 7;
+
+
+
+            //introduction
+            Console.WriteLine("                             -|             |-\r\n         -|                  [-_-_-_-_-_-_-_-]                  |-\r\n         [-_-_-_-_-]          |             |          [-_-_-_-_-]\r\n          | o   o |           [  0   0   0  ]           | o   o |\r\n           |     |    -|       |           |       |-    |     |\r\n           |     |_-___-___-___-|         |-___-___-___-_|     |\r\n           |  o  ]              [    0    ]              [  o  |\r\n           |     ]   o   o   o  [ _______ ]  o   o   o   [     | ----__________\r\n_____----- |     ]              [ ||||||| ]              [     |\r\n           |     ]              [ ||||||| ]              [     |\r\n       _-_-|_____]--------------[_|||||||_]--------------[_____|-_-_\r\n      ( (__________------------_____________-------------_________) )");
+
+            Console.WriteLine("Narrator: You are one of the children of the King of Hearts who you know as 'Karts'," +
+                "you are one of the King and Kingdoms well known protectors/caretaker.");
+            Console.WriteLine("You have been summoned by the King for what sounded like a small task nothing to serious.");
+            Console.WriteLine("Which of the Kings children are you?");
+
+            Console.WriteLine("");
+            
+
         }
+
     }
 }
